35f9feb1668662093b51c5f55b3d62b81385136c
Hazereal418 <adefolarin.adebowale@gmail.com>
2026-01-03 00:54:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ function SendMail(){
     var params = {
         message: document.getElementById("message").value,
     }
-    emailjs.send("service_4noo4g8","template_rwno42r", params);
+    emailjs.send("service_4noo4g8","template_rwno42r", params)then(document.write("<h1>An Error Occured, Please try again! </h1>")
+        
+    );
 }
